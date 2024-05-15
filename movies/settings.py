b15cc90a92bb58968ecbd3b87b67e7ac5ac2fc22
sjeevankumar <sjeevankumar89@gmail.com>
2024-05-15 16:48:06 +0530

    feat:course completed

--- movies/settings.py
+++ movies/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-6)(z9#a0-hd09g63h@r!aci$ngdmntf7_&jb0-57!g+)sk1x_b'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
