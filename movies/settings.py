eeb8cc2ec04e921ef34a6ae0272f430a1024c953
sjeevankumar <sjeevankumar89@gmail.com>
2024-05-15 13:10:15 +0530

    feat:first template and view

--- movies/settings.py
+++ movies/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-
+SECRET_KEY = 'django-insecure-6)(z9#a0-hd09g63h@r!aci$ngdmntf7_&jb0-57!g+)sk1x_b'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'movies',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
